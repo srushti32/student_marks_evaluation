4059cf449dea709361be6179eaffacde1dda5f28
srushti32 <hsrushti320@gmail.com>
2025-11-27 21:05:13 +0530

    update modify.py

---
+++ modify.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv) < 2:
+   print("usage: pyhton scores_local.py <s1> <s2> <s3>...")
+  sys.exit(1)
+
+scores = [float(x) for x in sys.argv[1:]]
+
+print("scores:",scores)
+print("sum:", sum(scores))
+print("average:", sum(scores) / len(scores))
+print("maximum:", max(scores))
+print("minimum:", min(scores))
